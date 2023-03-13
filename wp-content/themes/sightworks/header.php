3c4b40d00ce5ecac2635cf3aa19edeb29ee52204
iTanvir <thisistanvir.dev@gmail.com>
2023-03-13 17:09:41 +0600

    Some Fixing

--- wp-content/themes/sightworks/header.php
+++ wp-content/themes/sightworks/header.php
@@ -33,7 +33,7 @@
     <!-- header start -->
     <?php
     if (!function_exists('elementor_theme_do_location') || !elementor_theme_do_location('header')) {
-        get_template_part('./template-parts/header');
+        get_template_part('template-parts/header');
     }
     ?>
     <!-- end: Header Area -->
